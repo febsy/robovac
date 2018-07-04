3516626a48686e7c8c56ddfac690b75da859d511
febsy <fabian_schwager@hotmail.com>
2018-07-04 02:22:11 +0200

    fixed adc

--- robovac_modular_software/robovac_modular_software/adc.c
+++ robovac_modular_software/robovac_modular_software/adc.c
@@ -9,13 +9,15 @@
 
 uint8_t adc_init(void){
 	ADMUX = 0b00000000; //S.248 ff
-	ADCSRA = 0b11100000;
-	ADCSRB = 0b00000000;
+	ADCSRA |= (1<<ADPS2)|(1<<ADPS1)|(1<<ADPS0)|(1<<ADEN);
 	DIDR0 |= 0b00000001;
 	return 0;
 }
 
 int16_t adc_getvalue(void){
+	ADMUX = (ADMUX & 0xF0) | (0x00 & 0x0F);
+	ADCSRA |= (1<<ADSC);
+	while( ADCSRA & (1<<ADSC) );
 	unsigned char lval,hval;
 	lval = ADCL;
 	hval = ADCH;
